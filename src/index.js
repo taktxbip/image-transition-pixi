af2608f256cd5130c830f8439223d6c1f6efca04
Evnomad <b.boy.bip@gmail.com>
2022-04-24 17:06:04 +0400

    done

--- src/index.js
+++ src/index.js
@@ -5,29 +5,15 @@ import Shredder from './js/shredder';
 import './scss/main.scss';
 
 
-const slides = 5;
-
 (function () {
     window.addEventListener('DOMContentLoaded', (e) => {
         const args = {
             slides: 5,
             currentSlide: 0,
-            shreds: 50
+            shreds: 20,
+            speed: 0.3
         };
         const sh = new Shredder(args);
         sh.load();
-
-        // filter.apply = function (filterManager, input, output, clear) {
-        //     const matrix = new PIXI.Matrix();
-        //     this.uniforms.mappedMatrix = filterManager.calculateNormalizedScreenSpaceMatrix(matrix);
-        //     PIXI.Filter.prototype.apply.call(
-        //         this,
-        //         filterManager,
-        //         input,
-        //         output,
-        //         clear
-        //     );
-        // };
-
     });
 })();
--- src/js/shredder.js
+++ src/js/shredder.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import fragment from '../shader/fragment.glsl';
+import vertex from '../shader/vertex.glsl';
 import gsap from 'gsap';
 
 import img0 from '../assets/alexander-wende.jpg';
@@ -13,11 +14,11 @@ export default class Shredder {
 
         this.slides = args.slides;
         this.currentSlide = args.currentSlide;
-        this.prevSlide = args.currentSlide;
         this.shreds = args.shreds;
+        this.speed = args.speed;
         this.resources = null;
 
-        this.filter = new PIXI.Filter(null, fragment);
+        this.filter = new PIXI.Filter(vertex, fragment);
 
         this.app = new PIXI.Application({
             width: window.innerWidth,
@@ -49,9 +50,9 @@ export default class Shredder {
 
     events() {
         window.addEventListener('resize', () => {
-            console.log('resize');
-            console.log(this.currentSlide);
-            this.filter.uniforms.uvAspect = this.getAspectRatio(this.resources[`img${this.currentSlide}`]);
+            console.log('res');
+            this.filter.uniforms.uvAspectOne = this.getAspectRatio(this.resources[`img${this.currentSlide}`]);
+            this.filter.uniforms.uvAspectTwo = this.filter.uniforms.uvAspectOne;
         });
     }
 
@@ -63,43 +64,45 @@ export default class Shredder {
 
             const sprite = new PIXI.Sprite(resources.img0.texture);
 
-            this.filter.uniforms.uvAspect = this.getAspectRatio(resources.img4);
+            this.filter.uniforms.uvAspectOne = this.getAspectRatio(resources[`img${this.currentSlide}`]);
+            this.filter.uniforms.uvAspectTwo = this.filter.uniforms.uvAspectOne;
+            this.filter.uniforms.uShreds = this.shreds;
+            this.filter.uniforms.uTextureOne = resources[`img${this.currentSlide}`].texture;
+            this.filter.uniforms.uTextureTwo = this.filter.uniforms.uTextureOne;
             this.filter.uniforms.uProgress = 0;
+            this.filter.uniforms.uDirection = 1;
 
             const a = document.querySelectorAll('.nav a');
             a.forEach((el, i) => {
                 el.addEventListener('mouseover', () => {
+                    if (this.currentSlide === i) {
+                        return;
+                    }
                     gsap.to(this.filter.uniforms, {
+                        duration: this.speed,
                         uProgress: i,
                         ease: "power3.out",
+                        onComplete: () => {
+                            this.currentSlide = i;
+                            console.log(`Current: ${this.currentSlide}`);
+                        },
                         onUpdate: () => {
-                            const num = Math.floor(this.filter.uniforms.uProgress);
-                            this.filter.uniforms.uTextureOne = resources[`img${num}`].texture;
+                            let from = this.currentSlide;
+                            let to = i;
 
-                            const next = num + 1 === this.slides ? 0 : num + 1;
-                            this.filter.uniforms.uTextureTwo = resources[`img${next}`].texture;
+                            // console.log(`from: ${from} | to: ${to}`);
 
-                            this.currentSlide = num;
+                            this.filter.uniforms.uDirection = from < to ? 1 : -1;
 
-                            // const curAspect = getAspectRatio(resources[`img${num}`]);
-                            // const nextAspect = getAspectRatio(resources[next]);
+                            this.filter.uniforms.uTextureOne = resources[`img${from}`].texture;
+                            this.filter.uniforms.uvAspectOne = this.getAspectRatio(resources[`img${from}`]);
 
-                            // const time = filter.uniforms.uProgress - i + 1;
-                            // console.log(curAspect, nextAspect);
-
-                            // // filter.uniforms.uProgress
-                            // filter.uniforms.uvAspect = {
-                            //     x: nextAspect.x + (nextAspect.x - nextAspect.x) * time,
-                            //     y: nextAspect.y + (nextAspect.y - nextAspect.y) * time
-                            // };
+                            this.filter.uniforms.uTextureTwo = resources[`img${to}`].texture;
+                            this.filter.uniforms.uvAspectTwo = this.getAspectRatio(resources[`img${to}`]);
                         }
                     });
                 });
             });
-
-            this.filter.uniforms.uShreds = this.shreds;
-            this.filter.uniforms.uTextureOne = resources[`img0`].texture;
-            this.filter.uniforms.uTextureTwo = resources[`img1`].texture;
 
             // Add the sprite to the scene we are building
             sprite.filters = [this.filter];
@@ -128,8 +131,8 @@ export default class Shredder {
             };
         } else {
             return {
-                y: 1,
-                x: wAspect / iAspect
+                x: wAspect / iAspect,
+                y: 1
             };
         }
     }
